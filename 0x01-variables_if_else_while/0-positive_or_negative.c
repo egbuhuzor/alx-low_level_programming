61dd1e262936e020a74a038b20353cd09887f331
CharlesIro1125 <irocharles22@gmail.com>
2022-06-03 14:42:32 -0700

    updating command

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,11 +3,12 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/* the main funtion checks the random number and 
+ * output if sign or unsign  
+ * */
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
@@ -16,6 +17,5 @@ int main(void)
 		printf("%d is zero\n",n);
 	else 
 		printf("%d is negative\n",n);
-
 	return (0);
 }
