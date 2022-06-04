1216e7ec65bf9b18d8204892c3552dd77d725793
CharlesIro1125 <irocharles22@gmail.com>
2022-06-04 10:15:25 -0700

    Update the random function

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - prints the single digit numbers 
+ *
+ * Description: this function prints single 
+ * digit numbers of base ten
+ * Return: null
+ */
+int main(void)
+{
+	int i;
+	char num[] = {'0', '1', '2', '3', '4',
+		'5', '6', '7', '8', '9', '\n', '\0'};
+	int p = strlen(num);
+
+	for (i = 0; i < p; i++)
+		putchar(num[i]);
+	return (0);
+}
+
