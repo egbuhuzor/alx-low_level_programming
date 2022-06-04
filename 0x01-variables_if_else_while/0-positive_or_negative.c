bbd2a3dd3aaf69aff0bedfe01761dec567fd6bd1
CharlesIro1125 <irocharles22@gmail.com>
2022-06-04 00:05:07 -0700

    Update the random function

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,18 +5,21 @@
 /** 
  * main - checks the random number.
  *
- * description: the main funtion checks the random number * and output if sign or unsign.
+ * Description: the main funtion checks the random number * and output if sign or unsign.
+ * Return: null
  */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%d is positive\n",n);
+		printf("%d is positive\n", n);
 	else if (n == 0)
-		printf("%d is zero\n",n);
+		printf("%d is zero\n", n);
 	else 
-		printf("%d is negative\n",n);
+		printf("%d is negative\n", n);
+
 	return (0);
 }
