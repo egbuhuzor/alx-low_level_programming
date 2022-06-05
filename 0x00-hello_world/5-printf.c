66c8e337a8afd18421c97c5afee036ee07ccb01b
CharlesIro1125 <irocharles22@gmail.com>
2022-06-05 11:59:33 -0700

    update result

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: this function prints a text
+ * Return: Always zero Success
+ *
+ */
+int main(void)
+{
+	printf("%s\n","with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: this function prints variable sizes
+ * Return: Always zero Success
+ */
+int main(void)
+{
+	int b;
+	char ch;
+	long lg;
+	long long llg;
+	float fl;
+
+	printf("Size of a char: %d byte(s)\n",sizeof(ch));
+	printf("Size of a int: %d byte(s)\n",sizeof(b));
+	printf("Size of a long int: %d byte(s)\n",sizeof(lg));
+	printf("Size of a long long int: %d byte(s)\n",sizeof(llg));
+	printf("Size of a float: %d byte(s)\n",sizeof(fl));
+	return (0);
+}
