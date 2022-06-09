c566f2505352eff01df31a8404907070f5e0677b
egbuhuzor <swiftappsoft@gmail.com>
2022-06-09 07:53:58 -0700

    commit

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,24 +1,25 @@
 #include "main.h"
 
 /**
- * print_square - prints a squre followed by a new line
- * @size: size of the square
+ * print_square - print a square of #'s given size
+ * @size: size to draw
  */
+
 void print_square(int size)
 {
-	int row, col;
+int row, column;
 
-	if (size <= 0)
-		_putchar('\n');
-	else
-	{
-		for (row = 1; row <= size; row++)
-		{
-			for (col = 1; col <= size; col++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
-		}
-	}
+if (size > 0)
+{
+for (row = 1; row <= size; row++)
+{
+for (column = 1; column <= size; column++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else
+_putchar('\n');
 }
