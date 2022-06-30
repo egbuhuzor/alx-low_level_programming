28091cc33b86aca86ce5ed23f4134372fe426a6f
egbuhuzor <swiftappsoft@gmail.com>
2022-06-30 09:24:39 -0700

    commit

--- 0x08-malloc_free/0-create_array.c
+++ 0x08-malloc_free/0-create_array.c
@@ -1,28 +1,28 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-
 /**
- * create_array - creates n array of chars
- * @size: size of array
- * @c: char type
- * Return: base address of pointer
+ * create_array - creates an array of chars.
+ * @size: size of the array.
+ * @c: storaged char
+ *
+ * Return: pointer of an array of chars
  */
 char *create_array(unsigned int size, char c)
 {
-	char *a;
-	unsigned int b;
+	char *cr;
+	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
 
-	a = malloc(sizeof(char) * size);
+	cr = malloc(sizeof(c) * size);
 
-	if (a == NULL)
+	if (cr == NULL)
 		return (NULL);
 
-	for (b = 0; b < size; b++)
-		a[b] = c;
+	for (i = 0; i < size; i++)
+		cr[i] = c;
 
-	return (a);
+	return (cr);
 }
+
